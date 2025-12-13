fcb7cdac5597850972d804e278f7e79a14954531
Brandon-Butterbaugh <bjbutterbaugh@comcast.net>
2025-12-13 17:48:39 -0600

    implemented the thumbnail upload

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,58 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	// "thumbnail" should match the HTML form input name
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	mediaType := header.Header.Get("Content-Type")
+	defer file.Close()
+
+	// Read the file into a byte slice
+	bytes, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to io.ReadAll on file", err)
+		return
+	}
+
+	// Get video metadata
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Unable to find video", err)
+		return
+	}
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "User is not the video owner", err)
+		return
+	}
+
+	// Make and save a new thumbnail struct to the global map
+	thumbnail := thumbnail{
+		data:      bytes,
+		mediaType: mediaType,
+	}
+	videoThumbnails[video.ID] = thumbnail
+
+	// Update video metadata for new thumbnail URL
+	thumbURL := fmt.Sprintf(
+		"http://localhost:%s/api/thumbnails/%s",
+		cfg.port,
+		video.ID.String(),
+	)
+	video.ThumbnailURL = &thumbURL
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to update video's thumbnail URL", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
